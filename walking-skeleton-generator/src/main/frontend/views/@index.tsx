a8e6ed9cac3d0bb6a3c23ededb6503812bad1292
Petter Holmström <petter@vaadin.com>
2025-01-15 12:12:37 +0200

    Add simple UI to skeleton generator.

---
+++ walking-skeleton-generator/src/main/frontend/views/@index.tsx
@@ -0,0 +1,97 @@
+import { Button, Notification, RadioButton, RadioGroup, TextField } from '@vaadin/react-components';
+import { useSignal, useSignalEffect } from '@vaadin/hilla-react-signals';
+
+type UiFramework = 'FLOW' | 'REACT'
+
+const getContextPath = (): string => {
+  return document.querySelector('meta[name="context-path"]')?.getAttribute('content') || '';
+};
+
+const buildUrl = (path: string): string => {
+  const contextPath = getContextPath();
+  const cleanPath = path.startsWith('/') ? path.substring(1) : path;
+  return `${contextPath}/${cleanPath}`;
+};
+
+export default function ProjectGeneratorView() {
+  const groupId = useSignal('');
+  const artifactId = useSignal('');
+  const basePackage = useSignal('');
+  const uiFramework = useSignal<UiFramework>('FLOW');
+  const error = useSignal(false);
+  const requiredDataMissing = useSignal(true);
+
+  // TODO Should also validate the input of the fields
+  useSignalEffect(() => {
+    requiredDataMissing.value = groupId.value.length == 0
+      || artifactId.value.length == 0
+      || basePackage.value.length == 0;
+  });
+
+  const downloadProject = async () => {
+    error.value = false;
+
+    const projectConfiguration = {
+      artifactId: artifactId.value,
+      groupId: groupId.value,
+      basePackage: basePackage.value,
+      uiFramework: uiFramework.value
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(projectConfiguration)
+    };
+
+    const response = await fetch(buildUrl('/generate'), requestOptions);
+
+    if (!response.ok) {
+      error.value = true;
+      return;
+    }
+
+    const blob = await response.blob();
+    await downloadFile(blob, projectConfiguration.artifactId);
+  };
+
+  const downloadFile = async (blob: Blob, artifactId: string) => {
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${artifactId}.zip`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  };
+
+  return (
+    <div className="w-full h-full flex justify-center items-center bg-contrast-10">
+      <div className="bg-base shadow-m p-l rounded-l">
+        <h1>Walking Skeleton Generator</h1>
+        <div className="flex flex-col">
+          <TextField label="Maven Project Group ID" placeholder="Example: com.example.application"
+                     value={groupId.value}
+                     onValueChanged={evt => groupId.value = evt.detail.value}></TextField>
+          <TextField label="Maven Project Artifact ID" placeholder="Example: my-project"
+                     value={artifactId.value}
+                     onValueChanged={evt => artifactId.value = evt.detail.value}></TextField>
+          <TextField label="Java Base Package" placeholder="Example: com.example.application"
+                     value={basePackage.value}
+                     onValueChanged={evt => basePackage.value = evt.detail.value}></TextField>
+          <RadioGroup label="User Interface Framework" value={uiFramework.value}
+                      onValueChanged={evt => uiFramework.value = evt.detail.value as UiFramework}>
+            <RadioButton value="FLOW" label="Flow" />
+            <RadioButton value="REACT" label="React" />
+          </RadioGroup>
+          <Button disabled={requiredDataMissing.value} theme="primary" onClick={downloadProject}>Download
+            Project</Button>
+          <Notification opened={error.value} theme="error" position="top-center" onClosed={evt => error.value = false}>
+            Whops, this did not go as planned. Please try again later.
+          </Notification>
+        </div>
+      </div>
+    </div>
+  );
+}
--- walking-skeleton-generator/src/main/java/org/vaadin/walkingskeleton/generator/Application.java
+++ walking-skeleton-generator/src/main/java/org/vaadin/walkingskeleton/generator/Application.java
@@ -1,10 +1,27 @@
 package org.vaadin.walkingskeleton.generator;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Theme("default")
+public class Application implements AppShellConfigurator {
+
+    private final String contextPath;
+
+    public Application(@Value("${server.servlet.context-path}") String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        AppShellConfigurator.super.configurePage(settings);
+        settings.addMetaTag("context-path", contextPath);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
